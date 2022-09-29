e62551a42930bab6034d3e53311ea31b3871cbd9
简 <2447746158@qq.com>
2022-09-29 14:50:22 +0800

    define basic goal

--- app/Goal.hs
+++ app/Goal.hs
@@ -1,11 +1,15 @@
 module Goal (
-
+  Goal,
+  run,
+  equal,
+  conj,
+  disj,
 ) where
 
 import Value
 import Substitution
 
-import Data.Maybe
+import Data.Maybe ( maybeToList )
 
 type Goal = Substitution -> [Substitution]
 
@@ -14,3 +18,20 @@ run = ($)
 
 equal :: Value -> Value -> Goal
 equal x y sub = maybeToList $ unify x y sub
+
+succeed :: Goal
+succeed = return
+
+fail :: Goal
+fail = const []
+
+conj :: Goal -> Goal -> Goal
+conj g1 g2 sub = concatMap g2 (g1 sub)
+
+mixList :: [a] -> [a] -> [a]
+mixList [] ys = ys
+mixList xs [] = xs
+mixList (x:xs) ys = x : mixList ys xs
+
+disj :: Goal -> Goal -> Goal
+disj g1 g2 sub = mixList (g1 sub) (g2 sub)
