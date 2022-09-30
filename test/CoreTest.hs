2a3b5ba2cf7a12455a073346a5d7db1ea996f1a7
简 <2447746158@qq.com>
2022-09-30 20:14:37 +0800

    add some coreTest

--- test/CoreTest.hs
+++ test/CoreTest.hs
@@ -10,11 +10,35 @@ import qualified Stream as S
 import Test.HUnit
 import qualified Data.Map as Map
 
-bindTest :: Test
-bindTest = TestCase $
-  assertEqual "Bind Test" 
+bindTest1 :: Test
+bindTest1 = TestCase $
+  assertEqual "Bind Test 1" 
               (S.streamToList $ run (equal (Var "x") (Atom "a")) empty)
               [Map.fromList [("x", Atom "a")]]
 
+bindTest2 :: Test
+bindTest2 = TestCase $
+  assertEqual "Bind Test 2"
+              (S.streamToList $ run (equal (List [Var "x",  Var "y", Var "z"])
+                                           (List [Atom "a", Atom "b", Atom "c"]))
+                                    empty)
+              [Map.fromList [("x", Atom "a"), ("y", Atom "b"), ("z", Atom "c")]]
+
+failTest1 :: Test
+failTest1 = TestCase $
+  assertEqual "Fail Test 1" 
+              (S.streamToList $ run (equal (Atom "a") (Atom "b")) empty)
+              []
+
+failTest2 :: Test
+failTest2 = TestCase $
+  assertEqual "Fail Test 2" 
+              (S.streamToList . (`run` empty) $ 
+                foldl1 conj [
+                  equal (Var "x") (Atom "a")
+                , equal (Var "y") (Atom "b")
+                , equal (Var "x") (Var "y")])
+              []
+
 coreTest :: Test
-coreTest = TestList [bindTest]
+coreTest = TestList [bindTest1, bindTest2, failTest1, failTest2]
